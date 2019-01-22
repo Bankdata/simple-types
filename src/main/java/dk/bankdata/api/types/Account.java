8272e9c3634acc171f3d25c0876f0a6f535ed174
Kenneth Bøgedal <kbh@bankdata.dk>
2019-01-22 20:43:19 +0100

    Added account object

---
+++ src/main/java/dk/bankdata/api/types/Account.java
@@ -0,0 +1,56 @@
+package dk.bankdata.api.types;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Account {
+    private String regNo;
+    private String accountNo;
+    private String name;
+    private BigDecimal balance;
+
+    public String getRegNo() {
+        return regNo;
+    }
+
+    public int getRegNoAsInt() {
+        return Integer.valueOf(regNo);
+    }
+
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+        Account account = (Account) object;
+        return Objects.equals(regNo, account.regNo) &&
+                Objects.equals(accountNo, account.accountNo) &&
+                Objects.equals(name, account.name) &&
+                Objects.equals(balance, account.balance);
+    }
+
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), regNo, accountNo, name, balance);
+    }
+
+    @Override
+    public java.lang.String toString() {
+        return "Account{" +
+                "regNo='" + regNo + '\'' +
+                ", accountNo='" + accountNo + '\'' +
+                ", name='" + name + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}
