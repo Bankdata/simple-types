214095ae0d2540052f94e67da5629302e3b5830e
Thor Anker Kvisgård Lange <thor.lange@gmail.com>
2019-03-27 15:26:59 +0100

    :rotating_light: Remove checkstyle error.

--- src/test/java/dk/bankdata/api/types/AccountNumberTest.java
+++ src/test/java/dk/bankdata/api/types/AccountNumberTest.java
@@ -1,9 +1,9 @@
 package dk.bankdata.api.types;
 
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Test;
 
 public class AccountNumberTest {
 
