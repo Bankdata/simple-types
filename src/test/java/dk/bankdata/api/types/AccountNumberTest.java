096313606c42608d9fd756123a6ae38becf093a5
bogedal <kbh@bankdata.dk>
2019-07-02 13:40:13 +0200

    :hammer:Fixed checkstyle issues

--- src/test/java/dk/bankdata/api/types/AccountNumberTest.java
+++ src/test/java/dk/bankdata/api/types/AccountNumberTest.java
@@ -1,6 +1,8 @@
 package dk.bankdata.api.types;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import dk.bankdata.api.jaxrs.encryption.DecodingType;
