7099078b5413933d76401837aa403af2e5b4b76e
SilverGull <peter.gull@gmail.com>
2019-10-14 20:45:45 +0200

    unit tests

--- src/test/java/dk/bankdata/api/types/TradeTypeTest.java
+++ src/test/java/dk/bankdata/api/types/TradeTypeTest.java
@@ -1,9 +1,7 @@
 package dk.bankdata.api.types;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -11,16 +9,38 @@ import org.junit.jupiter.api.Test;
 
 public class TradeTypeTest {
 
-
     @Nested
     @DisplayName("byCharCode")
     class ByCharCode {
 
         @Test
         public void shouldGetTradeType() {
-            // Map<Character, String> charCodes = new HashMap<>('S', 'M', 'L');
+            assertEquals(TradeType.IMMEDIATE, TradeType.byCharCode('S').get());
+            assertEquals(TradeType.MARKET, TradeType.byCharCode('M').get());
+            assertEquals(TradeType.LIMIT, TradeType.byCharCode('L').get());
         }
 
+        @Test
+        public void shouldGetEmptyOptional() {
+            assertFalse(TradeType.byCharCode('X').isPresent());
+            assertFalse(TradeType.byCharCode('s').isPresent());
+        }
     }
 
+    @Nested
+    @DisplayName("byNumberCode")
+    class ByNumberCodBe {
+
+        @Test
+        public void shouldGetTradeType() {
+            assertEquals(TradeType.IMMEDIATE, TradeType.byNumberCode(1).get());
+            assertEquals(TradeType.MARKET, TradeType.byNumberCode(4).get());
+            assertEquals(TradeType.LIMIT, TradeType.byNumberCode(5).get());
+        }
+
+        @Test
+        public void shouldGetEmptyOptional() {
+            assertFalse(TradeType.byNumberCode(9).isPresent());
+        }
+    }
 }
